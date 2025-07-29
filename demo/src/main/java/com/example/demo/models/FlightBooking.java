af707e0ee7d2bf7fb2ed31adb38b554598fea307
Ramix76 <framos-p@student.42barcelona.com>
2025-07-29 14:11:58 +0200

    lab flight spring

---
+++ demo/src/main/java/com/example/demo/models/FlightBooking.java
@@ -0,0 +1,24 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightBooking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_Id")
+    private Customer customerId;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_Id")
+    private Flight flightId;
+}
