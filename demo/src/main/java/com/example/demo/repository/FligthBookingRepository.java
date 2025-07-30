03a20a0a78fa8519be271c32b0e56650a2178618
Ramix76 <framos-p@student.42barcelona.com>
2025-07-30 08:30:50 +0200

    change models folder name to model

--- demo/src/main/java/com/example/demo/repository/FligthBookingRepository.java
+++ demo/src/main/java/com/example/demo/repository/FligthBookingRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.models.FlightBooking;
+import com.example.demo.model.FlightBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FligthBookingRepository extends JpaRepository<FlightBooking, Long> {
